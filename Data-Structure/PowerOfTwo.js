391ec2c94370725179c7e252be5ffbca6fc9d248
MuhdHanish <muhammedhanish11@gmail.com>
2023-06-10 15:34:48 +0530

    bitwise operator added and the complexity reduced to O(1)

--- Data-Structure/PowerOfTwo.js
+++ Data-Structure/PowerOfTwo.js
@@ -1,16 +1,25 @@
 
 
-const powerOfTwo = (num) =>{ // O(1)
- while (num >= 0) { // O(logn) reducing the number half
-  num /= 2; // O(1)
-  if (num == 1) {// O(1)
-   return true; // O(1)
-  } else if (num < 1) { // O(1)
-   return false; // O(1)
-  }
- }
-}
+// const powerOfTwo = (num) =>{ // O(1)
+//  while (num >= 0) { // O(logn) reducing the number half
+//   num /= 2; // O(1)
+//   if (num == 1) {// O(1)
+//    return true; // O(1)
+//   } else if (num < 1) { // O(1)
+//    return false; // O(1)
+//   }
+//  }
+// }
 
-console.log(powerOfTwo(8))
+// console.log(powerOfTwo(8))
 
 // O(logn)
+
+const powerOfTwoBitWise = (num) => { // O(1)
+ if (num < 1) { // O(1)
+  return false; // O(1)
+ }
+ return (num & (num - 1)) === 0; // O(1)
+}
+
+console.log(powerOfTwoBitWise(3))
