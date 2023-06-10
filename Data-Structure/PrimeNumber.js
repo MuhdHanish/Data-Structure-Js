820bb9698b62a0712681676386bab8ede05390f4
MuhdHanish <muhammedhanish11@gmail.com>
2023-06-10 14:47:13 +0530

    Prime number funciton just changed

--- Data-Structure/PrimeNumber.js
+++ Data-Structure/PrimeNumber.js
@@ -1,5 +1,6 @@
 
 const checkPrime = (num) =>{ // O(1)
+ if (num == 1) return false // O(n)
  for (i = 2; i < num; i++) { // O(n)
   if (num % i == 0) { // O(1)
    return false; // O(1)
@@ -8,6 +9,6 @@ const checkPrime = (num) =>{ // O(1)
  return true; // O(1)
 }
 
-console.log(checkPrime(9))
+console.log(checkPrime(1))
 
-// O(n)
+// O(n) 
