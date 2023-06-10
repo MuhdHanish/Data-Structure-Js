9ff299b2cfdba73ba544df34e31c4c243d201ddb
MuhdHanish <muhammedhanish11@gmail.com>
2023-06-10 15:25:38 +0530

    Power of two checking funcition added and it complexity calculated get  O(logn)

---
+++ Data-Structure/PowerOfTwo.js
@@ -0,0 +1,16 @@
+
+
+const powerOfTwo = (num) =>{ // O(1)
+ while (num >= 0) { // O(logn) reducing the number half
+  num /= 2; // O(1)
+  if (num == 1) {// O(1)
+   return true; // O(1)
+  } else if (num < 1) { // O(1)
+   return false; // O(1)
+  }
+ }
+}
+
+console.log(powerOfTwo(8))
+
+// O(logn)
