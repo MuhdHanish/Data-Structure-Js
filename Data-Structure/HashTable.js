fcfab151588e49bc5f3a73e165696e8d3a76a533
MuhdHanish <muhammedhanish11@gmail.com>
2023-06-12 21:01:41 +0530

    hashtable collision handled using seperate chaining

--- Data-Structure/HashTable.js
+++ Data-Structure/HashTable.js
@@ -12,15 +12,41 @@ class HashTable {
  }
  Set(key, value) {
   const index = this.hash(key);
-  this.table[index] = value;
+  // this.table[index] = value;
+  const bucket = this.table[index];
+  if(!bucket){
+   this.table[index] = [key,value]
+  }else{
+   const sameKeyItem = bucket.find(item=>item[0]===key)
+   if(sameKeyItem){
+    sameKeyItem[1] = value
+   }else{
+    bucket.push(key,value)
+   }
+  }
  }
  get(key) {
   const index = this.hash(key);
-  return this.table[index]
+  // return this.table[index]
+  const bucket = this.table[index];
+  if(bucket){
+   const sameKeyItem = bucket.find(item=>item[0]===key)
+   if(sameKeyItem){
+    return sameKeyItem[1]
+   }
+  }
+  return undefined;
  }
  remove(key) {
   const index = this.hash(key);
-  this.table[index] = undefined;
+  // this.table[index] = undefined;
+  const bucket = this.table[index];
+  if(bucket){
+   const sameKeyItem = bucket.find(item=>item[0]===key)
+   if(sameKeyItem){
+    bucket.splice(bucket.indexOf(sameKeyItem),1)
+   }
+  }
  }
  display() {
   for (let i = 0; i < this.table.length; i++) {
