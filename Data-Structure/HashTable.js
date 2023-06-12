b2471ddf270332886e3c8e6bd94601fbcf503c8c
MuhdHanish <muhammedhanish11@gmail.com>
2023-06-12 20:37:24 +0530

    Hashtable implemnted

---
+++ Data-Structure/HashTable.js
@@ -0,0 +1,37 @@
+class HashTable {
+ constructor(size) {
+  this.table = new Array(size);
+  this.size = size;
+ }
+ hash(key) {
+  let total = 0;
+  for (let i = 0; i < key.length; i++) {
+   total += key.charCodeAt(i);
+  }
+  return total % this.size;
+ }
+ Set(key, value) {
+  const index = this.hash(key);
+  this.table[index] = value;
+ }
+ get(key) {
+  const index = this.hash(key);
+  return this.table[index]
+ }
+ remove(key) {
+  const index = this.hash(key);
+  this.table[index] = undefined;
+ }
+ display() {
+  for (let i = 0; i < this.table.length; i++) {
+   if (this.table[i]) {
+    console.log(i, this.table[i])
+   }
+  }
+ }
+}
+
+const table = new HashTable(50);
+table.Set('name','Bruce');
+table.Set('age',25);
+table.display()
